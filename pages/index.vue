ae8cab61dae921e5c496dad19f2df3a047c6008f
masbri <fatliciousman@gmail.com>
2024-09-13 16:03:01 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-row>
+    <v-col cols="8" class="frame-content px-6">
+      <products></products>
+    </v-col>
+    <v-col cols="4" class="frame-content white">
+      <products></products>
+    </v-col>
+  </v-row>
+</template>
